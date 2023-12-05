fce9014a142eb5a410f0af9b7e982e0e81e9bbdb
pc <djop1212@gmail.com>
2023-12-05 16:25:01 +0900

    test

---
+++ gongsik-web/src/main/java/com/gongsik/gsw/GongsikWebApplication.java
@@ -0,0 +1,13 @@
+package com.gongsik.gsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongsikWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongsikWebApplication.class, args);
+	}
+
+}
