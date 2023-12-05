2e7b56bdd2ff20dfbdefe9d4dcd2bd6761648421
pc <djop1212@gmail.com>
2023-12-05 17:06:01 +0900

    test

---
+++ gongsik-web/src/test/java/com/gongsik/gsw/GongsikWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongsik.gsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongsikWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
