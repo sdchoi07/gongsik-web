50fc3ef91f8138bf28d670f633dac427143c5987
pc <djop1212@gmail.com>
2023-12-15 22:40:39 +0900

    2023.12.15 <환경설정 및 error페이지 추가>

--- gongsik-web/src/main/java/com/gongsik/gsw/common/contoller/Common.java
+++ gongsik-web/src/main/java/com/gongsik/gsw/common/contoller/Common.java
@@ -1,18 +1,23 @@
 package com.gongsik.gsw.common.contoller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
-public class Common {
+public class Common implements ErrorController{
 	
 	    @GetMapping("/main")
 	    public String sayHello(Model model) {
 	        model.addAttribute("say", "Hello");
 	        return "main/main";
+	    }
+	    
+	    @GetMapping("/error")
+	    public String error() {
+	        return "main/error";
 	    }
 	}
 
--- gongsik-web/src/test/java/com/gongsik/gsw/GongsikWebApplicationTests.java
+++ gongsik-web/src/test/java/com/gongsik/gsw/GongsikWebApplicationTests.java
@@ -1,13 +1,23 @@
 package com.gongsik.gsw;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class GongsikWebApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		
+			mvc.perform(get("/main")).andDo(print());
 	}
-
 }
