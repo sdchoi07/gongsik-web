cc82791d122f72792d25c3640299f13c44150f8b
sdchoi <djop1212@gmail.com>
2024-01-06 16:43:19 +0900

    2024.01.06 <회원가입폼 검증>

--- gongsik-web/src/main/resources/static/assets/js/account/join.js
+++ gongsik-web/src/main/resources/static/assets/js/account/join.js
@@ -36,33 +36,57 @@ var _join = function() {
 	//국제 번호 조회
 	_countryPhList(restServer);
 	
+	//휴대폰 검증및 포맷 변환
+	_phoneChk();
+	
 	//인증번호 요청
 	$('#authReq').on('click',function(event){
-		console.log("인증버튼");
 		event.preventDefault();
 		_authNumReq(restServer);
 	});
+	
+	//회원가입 버튼
+	$('#joinBtn').on('click', function(event) {
+		 event.preventDefault();
+		_joinBtn();
+	})
 }
 
 //이메일 중복 확인 체크
 var _emailChkBtn = function() {
     //입력한 이메일 값
-	var emailValue = $('#email').val() + '@' + $('#domainTxt').val();
-	console.log(emailValue);
-	$.ajax({
-		url: "api/account/join/emailChk",
-		type: 'POST',
-		data: { usrId: emailValue },
-	}).success(function(data) {
-		if(data.code === 'suceess' ){
-			var insertHtml = '<span>중복체크 완료 하였습니다.</span>'
-		}else{
-			var insertHtml = '<span>이미 가입된 정보 입니다.</span>'
-		}
-			$('#idForm').html(insertHtml);
-	}).fail(function(data) {
+    var usrEmail = $('#usrEmail').val();
+    var domainTxt = $('#domainTxt').val();
+	var emailValue = usrEmail + '@' + domainTxt;
 	
-	});
+	//한글 검증
+	var pattern = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+	var koreanText = pattern.test(usrEmail);
+	//url 특수문자 인코딩
+	var usrId = encodeURIComponent(emailValue);
+	if(koreanText){
+		alert("이메일 형식이 틀렸습니다.")
+	}else{
+		if(usrEmail !== '' && domainTxt !== '' ){
+			$.ajax({
+				url: "/api/account/join/emailChk/"+usrId,
+				type: 'GET',
+			    contentType: 'application/json',
+			}).done(function(data) {
+					$('#doubleChk').text("")
+				if(data.code === 'success' ){
+					alert(data.msg);
+				}else{
+					alert(data.msg);
+				}
+			}).fail(function(jqXHR, textStatus, errorThrown) {
+		        console.error("AJAX request failed: " + textStatus +  errorThrown);
+		        alert("오류입니다.")
+		    });
+	    }else{
+			alert("이메일을 입력해 주세요.")
+		}
+	}
 }
 
 //비밀번호 입력 체크
@@ -138,6 +162,22 @@ var _birthFormat = function(){
         });
 }
 
+//폰 번호 체크 및 변환
+var _phoneChk = function(){
+        $('#phoneNumber').on('input', function() {
+            var inputValue = $(this).val().replaceAll('-', '');
+	        var numbersOnly = inputValue.replace(/[^\d]/g, ''); // 숫자 이외의 문자 제거
+	
+	      if (inputValue.length === 11 && /^\d+$/.test(numbersOnly)) {
+            var formattedDate = numbersOnly.replace(/^(\d{3})(\d{4})(\d{4})$/, '$1$2$3');
+            var phoneNo = formattedDate;
+            $(this).val(phoneNo); // 변환된 번호를 입력란에 적용
+        } else if (inputValue.length > 11 || numbersOnly.length > 11) {
+            $(this).val(inputValue.slice(0, 11)); // 11자리까지만 입력되도록 설정
+        }
+	    });
+}
+
 //국제 번호 조회
 var _countryPhList = function(){
 	$.ajax({
@@ -167,6 +207,26 @@ var _countryPhList = function(){
 //인증번호 요청
 var _authNumReq = function(){
 	var joinData = $('#joinForm').serializeObject();
+	var phoneNumber = joinData.phoneNumber;
+	var countryPhNo = joinData.countryPhNo;
+	if(countryPhNo === '' || countryPhNo === 'undefined'){
+		alert("국제번호 선택 해주세요.");
+		return;
+	}
+	if(phoneNumber === '' || phoneNumber === 'undefined'){
+		alert("휴대폰 번호를 입력해 주세요.");
+		return;
+	}
+	if(phoneNumber.length !== 11){
+		alert("휴대폰 번호를 제대로 입력해 주세요.");
+		return;
+	}
+	var pattern = /^\d+$/.test(phoneNumber);
+	if(!pattern){
+		alert("숫자로만 입력해주세요.");
+		return;
+	}
+	
 	$.ajax({
 		url : "/util/sendSMS",
 	    type: 'POST',
@@ -181,6 +241,26 @@ var _authNumReq = function(){
 		}
 	})
 }
+
+//인증번호 요청
+var _joinBtn = function(){
+	var joinData = $('#joinForm').serializeObject();
+	
+	$.ajax({
+		url : "/api/account/join",
+	    type: 'POST',
+        data: JSON.stringify(joinData), // form 데이터를 JSON 문자열로 변환하여 전송
+        contentType: 'application/json',
+	}).done(function(data){
+		console.log(data.msg + " " + data.code);
+		if(data.code === 'success'){
+			alert(data.msg);
+		}else{
+			alert(data.msg);
+		}
+	})
+}
+
 
 
 
